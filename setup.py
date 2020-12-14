eb9fe40f040645169048da509430f8a54594ebe6
Phil Wang <lucidrains@gmail.com>
2020-12-14 11:30:40 -0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'adjacent-attention-pytorch',
   packages = find_packages(),
-  version = '0.0.7',
+  version = '0.0.8',
   license='MIT',
   description = 'Adjacent Attention Network - Pytorch',
   author = 'Phil Wang',
